cda1d7caf4b6e42be7bdc4bd04d382e6521df3b4
Ilya <Forest_Game_Channele@mail.ru>
2022-05-15 14:28:50 +0300

    Append glade system, Debug Gtk

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 //
 // Created by Ilya on 15.05.2022.
 //
+#include "gtkmm.h"
+#include <iostream>
 
-#include "iostream"
 
-int main() {
-    std::cout << "Hello World!";
+int main(int argc, char **argv) {
+    auto app = Gtk::Application::create(argc, argv);
+    auto ui = Gtk::Builder::create_from_file("design.glade");
+
+    Gtk::ApplicationWindow *window;
+    ui->get_widget("window", window);
+
+    Gtk::Box *NoteBook;
+    ui->get_widget("NoteBook", NoteBook);
+
+    return app->run(*window);
 }
