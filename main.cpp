559db067f8e75198ef7ec23349b09ec30edf5a11
Ilya <Forest_Game_Channele@mail.ru>
2022-05-17 08:40:35 +0300

    class work

--- main.cpp
+++ main.cpp
@@ -11,9 +11,35 @@ int main(int argc, char **argv) {
 
     Gtk::Box *main_box;
     ui->get_widget("main_box", main_box);
-    Gtk::Entry *input = ;
-    ui->get_widget("input", input);
-    main_box.add(input);
+
+    Gtk::Label lbl("1");
+    main_box->add(lbl);
+    lbl.show();
+
+    Gtk::Entry input;
+    main_box->add(input);
+    input.show();
+
+    Gtk::Box button_box;
+    Gtk::Button del_btn("Удалить");
+    Gtk::Button left_btn("<-");
+    Gtk::Button save_btn("Сохранить");
+    Gtk::Button right_btn("->");
+    Gtk::Button create_btn("Создать");
+
+    button_box.add(del_btn);
+    button_box.add(left_btn);
+    button_box.add(save_btn);
+    button_box.add(right_btn);
+    button_box.add(create_btn);
+    main_box->add(button_box);
+
+    del_btn.show();
+    left_btn.show();
+    save_btn.show();
+    right_btn.show();
+    create_btn.show();
+    button_box.show();
 
     return app->run(*window);
 }
